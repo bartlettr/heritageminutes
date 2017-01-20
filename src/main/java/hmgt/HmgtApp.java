2b8470227bebda2846130a87ea5afb30925ff5c8
James Ellwood <jellwood@gmail.com>
2017-01-20 21:50:40 +0000

    Add bare spring boot app.

---
+++ src/main/java/hmgt/HmgtApp.java
@@ -0,0 +1,13 @@
+package hmgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HmgtApp {
+
+	public static void main(final String[] args) throws Exception {
+		SpringApplication.run(HmgtApp.class, args);
+	}
+
+}
