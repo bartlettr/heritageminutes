0403355f5bcbf146ffd227b6685ec34d0ec97fd1
James Ellwood <jellwood@gmail.com>
2017-03-29 20:44:08 -0400

    Add the message and info boxes, and probably some other things.

--- src/main/webapp/app/components/map/map.controller.js
+++ src/main/webapp/app/components/map/map.controller.js
@@ -5,10 +5,16 @@
         .module('HmgtApp')
         .controller('MapController', MapController);
 
-    MapController.$inject = ['$scope', '$http'];
+    MapController.$inject = ['$scope', '$compile', '$http'];
 
-    function MapController($scope, $http) {
+    function MapController($scope, $compile, $http) {
         var vm = this;
+
+        $scope.message = {};
+        $scope.message.hidden = false;
+
+        $scope.info = {};
+        $scope.info.visible = false;
 
         vm.map =  L.map('map', {scrollWheelZoom: false, zoomControl: false}).setView([57, -93], 4);
         L.control.zoom({position: 'bottomright'}).addTo(vm.map);
@@ -18,6 +24,20 @@
                 '&copy; <a href="https://carto.com/attribution">CARTO</a>',
             maxZoom: 18,
         }).addTo(vm.map);
+
+        var messageControl = new L.control({ position: 'topleft' });
+        messageControl.onAdd = function (map) {
+            return createControl(messageControl,
+                'app/components/map/message.html');
+        };
+        messageControl.addTo(vm.map);
+
+        var infoControl = new L.control({ position: 'topleft' });
+        infoControl.onAdd = function (map) {
+            return createControl(infoControl,
+                'app/components/map/info.html');
+        };
+        infoControl.addTo(vm.map);
 
         vm.markers = L.markerClusterGroup({showCoverageOnHover: false, maxClusterRadius: 20});
         vm.map.addLayer(vm.markers);
@@ -30,33 +50,71 @@
                     var locationUrl = '/api/minutes/' + minute.id + '/locations?group=default';
                     $http.get(locationUrl).then(addLocation);
                 }
-             }
-         );
+            }
+        );
 
-         function getMinute(id) {
+        function getMinute(id) {
             return vm.minutes.find(function(element) {
                 return element.id === id;
             });
-         }
+        }
 
-         function addLocation(response) {
-             var location = response.data;
-             if(location) {
-                var markerColour = location.dateCompleted ? 'green' : 'blue';
+        function addLocation(response) {
+            var location = response.data;
+            if(location) {
+                var markerColour = location.dateCompleted ? 'green-light' : 'blue';
                 var markerIcon = L.ExtraMarkers.icon({
-                     markerColor: markerColour,
-                     shape: 'circle'
-                 });
+                    markerColor: markerColour,
+                    shape: 'circle'
+                });
 
-                 var marker = new L.marker([location.lat, location.lng], {icon: markerIcon});
-                 vm.markers.addLayer(marker);
+                var marker = new L.marker([location.lat, location.lng], {icon: markerIcon});
+                vm.markers.addLayer(marker);
 
-                 var minute = getMinute(location.minuteId);
+                marker.on('click', function() {
+                    var minute = this.minute;
+                    var location = this.location;
 
-                 var tooltip = L.tooltip().setContent("<b>Minute:</b> " + minute.name +
-                    "<br/><b>Location:</b> " + location.name);
-                 marker.bindTooltip(tooltip);
-             }
-         }
+                    $scope.$apply(function() {
+                        $scope.message.hidden = true;
+                        $scope.info.minute = minute;
+                        $scope.info.location = location;
+                        $scope.info.visible = true;
+                    });
+                });
+
+                var minute = getMinute(location.minuteId);
+
+                marker.location = location;
+                marker.minute = minute;
+
+                addTooltip(marker, location, minute);
+            }
+        }
+
+        function addTooltip(marker, location, minute) {
+            var tooltip = L.tooltip().setContent('<div class="tooltip"><div class="tooltip-row"><span>Minute</span><span>' + minute.name +
+                '</span></div><div class="tooltip-row"><span>Location</span><span>' + location.name + "</span></div></div>");
+            marker.bindTooltip(tooltip);
+        }
+
+        function createControl(control, template) {
+            control._container = L.DomUtil.create('div');
+            angular.element(document).ready(function() {
+                var element = angular.element(control._container);
+                element.html("<ng-include src=\"'" + template + "'\"></ng-include>");
+                $compile(element)($scope);
+                $scope.$apply();
+            });
+            return control._container;
+        }
+
+        $scope.closeMessage = function() {
+            $scope.message.hidden = true;
+        }
+
+        $scope.closeInfo = function() {
+            $scope.info.visible = false;
+        }
     }
 })();
